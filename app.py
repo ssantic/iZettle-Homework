67e96a4aa8b758f11ba4f646343109b38c109a90
ssantic <srdjan.santic@gmail.com>
2020-04-05 18:50:31 +0200

    Amend server to run multiple models.

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def makecalc():
 
 
 if __name__ == '__main__':
-    modelfile = 'rf_model.pickle'
-    model = p.load(open(modelfile, 'rb'))
+    model0 = p.load(open(rf_model_0, 'rb'))
+    model1 = p.load(open(rf_model_1, 'rb'))
+    model2 = p.load(open(rf_model_2, 'rb'))
     app.run(debug=True, host='0.0.0.0')
