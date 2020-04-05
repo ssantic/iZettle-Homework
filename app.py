b3994b29ed3b715f4eeaaf877cda389433bfe5e4
ssantic <srdjan.santic@gmail.com>
2020-04-05 09:19:56 +0200

    Add Flask API.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle as p
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/', methods=['POST'])
+def makecalc():
+    data = request.get_json()
+    prediction = np.array2string(model.predict(data))
+
+    return jsonify(prediction)
+
+
+if __name__ == '__main__':
+    modelfile = 'rf_model.pickle'
+    model = p.load(open(modelfile, 'rb'))
+    app.run(debug=True, host='0.0.0.0')
